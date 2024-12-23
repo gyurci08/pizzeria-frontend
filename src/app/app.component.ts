34ded7bfe3c4f878026530b179e298c1544fbcb0
gyoergy.jandzso@t-systems.com <haver1036@gmail.com>
2024-12-23 14:55:03 +0100

    Don't show navbar on specific pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './core/components/navbar/navbar.component';
+import {NgIf} from '@angular/common';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    NgIf
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pizzeria-frontend';
+
+  showNavbar: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showNavbar = ![
+          '/login',
+          '/logout'
+        ].includes(event.url);
+      });
+  }
 }
