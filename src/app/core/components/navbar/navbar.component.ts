ecce3eec91cfe477ee36754de52e3356456d2ebd
gyoergy.jandzso@t-systems.com <haver1036@gmail.com>
2024-12-29 10:07:27 +0100

    Logout nav-item

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {RouterLink, RouterModule} from '@angular/router';
+import {AuthenticationService} from '../../../authentication/service/authentication-service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -15,11 +17,21 @@ import {RouterLink, RouterModule} from '@angular/router';
     MatMenuItem,
     MatMenuTrigger,
     RouterLink,
-    RouterModule
+    RouterModule,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthenticationService) {}
+
+  // TODO: Pizza list and register for viewers
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 
 }
