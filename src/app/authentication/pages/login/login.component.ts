745179522912d47706d4d207a2cfa116414d7722
gyurci08 <haver1036@gmail.com>
2024-12-29 19:36:36 +0100

    Login: Backend is checked

--- src/app/authentication/pages/login/login.component.ts
+++ src/app/authentication/pages/login/login.component.ts
@@ -6,10 +6,12 @@ import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {CommonModule} from '@angular/common';
-import { catchError, tap } from 'rxjs/operators';
+import {catchError, finalize, tap} from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import {MatIcon} from '@angular/material/icon';
 import {Router} from '@angular/router';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -26,7 +28,8 @@ import {Router} from '@angular/router';
     MatError,
     MatIcon,
     MatIconButton,
-    MatLabel
+    MatLabel,
+    MatProgressSpinner
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -35,6 +38,8 @@ export class LoginComponent{
   loginForm: FormGroup;
   loginError: string | null = null;
   hidePassword = true;
+  isLoading = false;
+
 
   constructor(
     private fb: FormBuilder,
@@ -50,16 +55,31 @@ export class LoginComponent{
   onSubmit() {
     if (this.loginForm.valid) {
       const { username, password } = this.loginForm.value;
+      this.isLoading = true;
+      this.loginError = null; // Reset error message
       this.authService.login(username, password).pipe(
         tap(() => {
           console.log('Login successful');
           this.router.navigate(['/dashboard']);
         }),
-        catchError((error) => {
-          this.loginError = 'Invalid username or password';
+        catchError((error: HttpErrorResponse) => {
+          console.error('Login error:', error);
+          if (error.status === 0) {
+            this.loginError = 'Unable to connect to the server. Please check your internet connection.';
+          } else if (error.status === 403) {
+            this.loginError = 'Invalid username or password.';
+          } else {
+            this.loginError = 'An unexpected error occurred. Please try again later.';
+          }
           return throwError(() => error);
+        }),
+        finalize(() => {
+          this.isLoading = false;
         })
       ).subscribe();
     }
   }
+
+
+
 }
